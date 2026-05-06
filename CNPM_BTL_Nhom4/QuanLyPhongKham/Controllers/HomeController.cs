2dd85d1d1ee36301f0d17e8e087c73dc5374113e
Huyentrang03789 <duongthtrang06@gmail.com>
2026-05-06 16:46:32 +0700

    Update HomeController.cs

--- CNPM_BTL_Nhom4/QuanLyPhongKham/Controllers/HomeController.cs
+++ CNPM_BTL_Nhom4/QuanLyPhongKham/Controllers/HomeController.cs
@@ -1,32 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-using QuanLyPhongKham.Models;
-using System.Diagnostics;
 
-namespace QuanLyPhongKham.Controllers
+namespace FamilyDoctorMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            if (HttpContext.Session.GetString("user") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
