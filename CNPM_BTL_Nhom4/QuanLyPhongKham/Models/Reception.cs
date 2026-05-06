6bb86de7c688a964d9f68c976f45734bdc741eae
Huyentrang03789 <duongthtrang06@gmail.com>
2026-05-06 16:45:26 +0700

    Add Reception model with properties

---
+++ CNPM_BTL_Nhom4/QuanLyPhongKham/Models/Reception.cs
@@ -0,0 +1,17 @@
+namespace FamilyDoctorMVC.Models
+{
+    public class Reception
+    {
+        public int ReceptionId { get; set; }
+
+        public string ReceptionName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
