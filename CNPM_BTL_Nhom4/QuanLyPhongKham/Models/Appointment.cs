4cdd10fe3d28773bfcdb2574ff17927dc94d3897
Huyentrang03789 <duongthtrang06@gmail.com>
2026-05-06 16:40:09 +0700

    Update Appointment.cs

---
+++ CNPM_BTL_Nhom4/QuanLyPhongKham/Models/Appointment.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FamilyDoctorMVC.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+
+        public int PatientId { get; set; }
+
+        public string DoctorUsr { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+    }
+}
